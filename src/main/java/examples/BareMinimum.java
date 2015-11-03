1a197eb18ee785366f1bdf8d0ba74dae6340b9c2
keith <KeithRieck@gmail.com>
2015-11-02 21:30:31 -0600

    Update client to work with OpcProxy

--- src/main/java/examples/BareMinimum.java
+++ src/main/java/examples/BareMinimum.java
@@ -19,7 +19,10 @@ public class BareMinimum extends Animation {
 		String FC_SERVER_HOST = System.getProperty("fadecandy.server", "raspberrypi.local");
 		int FC_SERVER_PORT = Integer.parseInt(System.getProperty("fadecandy.port", "7890"));
 		int PIXELSTRIP_PIN = Integer.parseInt(System.getProperty("pixelStrip", "0"));
+		boolean verbose = "true".equalsIgnoreCase(System.getProperty("verbose", "false"));
 		OpcClient server = new OpcClient(FC_SERVER_HOST, FC_SERVER_PORT);
+		server.setVerbose(verbose);
+		server.setSingleStripNum(PIXELSTRIP_PIN);
 		OpcDevice fadeCandy = server.addDevice();
 
 		PixelStrip strip1 = fadeCandy.addPixelStrip(PIXELSTRIP_PIN, 8);
