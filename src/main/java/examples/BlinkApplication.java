1e2d2e0f142b43eabec70ba8c5d80bc780cadad5
KeithRieck <KeithRieck@gmail.com>
2016-11-22 16:22:49 -0600

    tweaked system parameters

--- src/main/java/examples/BlinkApplication.java
+++ src/main/java/examples/BlinkApplication.java
@@ -5,15 +5,17 @@ import opc.*;
 public class BlinkApplication {
 
     public static void main(String[] args) throws Exception {
-		String FC_SERVER_HOST = System.getProperty("fadecandy.server", "raspberrypi.local");
-		int FC_SERVER_PORT = Integer.parseInt(System.getProperty("fadecandy.port", "7890"));
-		int PIXELSTRIP_PIN = Integer.parseInt(System.getProperty("pixelStrip", "0"));
+		final String FC_SERVER_HOST = System.getProperty("fadecandy.server", "raspberrypi.local");
+		final int FC_SERVER_PORT = Integer.parseInt(System.getProperty("fadecandy.port", "7890"));
+		final int STRIP1_COUNT = Integer.parseInt(System.getProperty("fadecandy.strip1.count", "8"));
+		final int PIXELSTRIP_PIN = Integer.parseInt(System.getProperty("pixelStrip", "0"));
+		final boolean VERBOSE = "true".equalsIgnoreCase(System.getProperty("verbose", "false"));
 		
         OpcClient server = new OpcClient(FC_SERVER_HOST, FC_SERVER_PORT);
 		server.setSingleStripNum(PIXELSTRIP_PIN);
         OpcDevice fadecandy = server.addDevice();
         
-        PixelStrip strip = fadecandy.addPixelStrip(PIXELSTRIP_PIN, 8);
+        PixelStrip strip = fadecandy.addPixelStrip(PIXELSTRIP_PIN, STRIP1_COUNT);
 
         for (int i = 0; i<100; i++) {
 	        strip.setPixelColor(0, 0x00FF00);
